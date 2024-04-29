2abacff9eb3bbc66001760fbf9f538c727a7a0ab
RobPruzan <rapruzan@buffalo.edu>
2024-04-28 22:42:19 -0400

    setup theming

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { run } from "@/lib/utils";
+
+export const ThemeToggle = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setTheme("dark");
+        }}
+        className="flex dark:hidden"
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+        <span className="sr-only">Switch to dark theme</span>
+      </Button>
+
+      <Button
+        onClick={() => {
+          setTheme("light");
+        }}
+        className="hidden dark:flex"
+      >
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+        <span className="sr-only">Switch to light theme</span>
+      </Button>
+    </>
+  );
+};
