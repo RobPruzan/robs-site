2ec193bfc16ba11ef17ea66363195bdab6aae9e1
RobPruzan <rapruzan@buffalo.edu>
2024-07-28 09:29:52 -0700

    simplify ts example

--- app/blog/(articles)/a-different-way-to-think-about-typescript/snippets/first-letter-uppercase-ts.ts
+++ app/blog/(articles)/a-different-way-to-think-about-typescript/snippets/first-letter-uppercase-ts.ts
@@ -2,16 +2,13 @@ const separateFirstWord = (t: string) => {
   const [firstWord, ...restWords] = t.split(" ");
   return [firstWord, restWords.join(" ")];
 };
-const uppercase = (char: string) => {
-  return char.toUpperCase();
-};
 const firstLetterUppercase = (t: string): string => {
   if (t.length === 0) {
     // base case
     return "";
   }
   const [firstWord, restWords] = separateFirstWord(t);
-  return `${uppercase(firstWord[0])}${firstWord.slice(
+  return `${firstWord[0].toUpperCase()}${firstWord.slice(
     1
   )} ${firstLetterUppercase(restWords)}`; // recursive call
 };
