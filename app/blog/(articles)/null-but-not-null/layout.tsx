1bea4ce9391520574801fb00a47e111a95081741
RobPruzan <rapruzan@buffalo.edu>
2024-09-22 18:51:30 -0400

    never can be asserted

--- app/blog/(articles)/null-but-not-null/layout.tsx
+++ app/blog/(articles)/null-but-not-null/layout.tsx
@@ -2,20 +2,17 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "null, but also not null",
-  description:
-    "How the empty set can be useful in typescript",
+  description: "How the empty set can be useful in typescript",
   openGraph: {
     title: "null, but also not null",
-    description:
-      "How the empty set can be useful in typescript",
+    description: "How the empty set can be useful in typescript",
     type: "article",
     publishedTime: "2024-09-22",
   },
   twitter: {
     card: "summary",
     title: "null, but also not null",
-    description:
-      "How the empty set can be useful in typescript",
+    description: "How the empty set can be useful in typescript",
   },
 };
 
