ece2bea54fcb08a23d041bb4158eb20c93ebcb48
RobPruzan <rapruzan@buffalo.edu>
2025-05-02 02:58:46 -0400

    later

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const ReactComponentName = require("react-scan/react-component-name/webpack").default; 
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  webpack: (config) => {
-    config.plugins.push(ReactComponentName({})); 
-    return config;
-  },
 });
