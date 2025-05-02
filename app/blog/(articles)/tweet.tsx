edafc91adced9e426e3fba56393cf0a45eda4eee
RobPruzan <rapruzan@buffalo.edu>
2025-05-02 03:02:09 -0400

    im stupid, maybe

--- app/blog/(articles)/tweet.tsx
+++ app/blog/(articles)/tweet.tsx
@@ -1,21 +1,20 @@
-"use client"
-import { Suspense } from 'react'
-import { unstable_cache } from 'next/cache'
-import { TweetSkeleton, EmbeddedTweet, TweetNotFound } from 'react-tweet'
-import { getTweet as _getTweet } from 'react-tweet/api'
- 
+import { Suspense } from "react";
+import { unstable_cache } from "next/cache";
+import { TweetSkeleton, EmbeddedTweet, TweetNotFound } from "react-tweet";
+import { getTweet as _getTweet } from "react-tweet/api";
+
 const getTweet = unstable_cache(
   async (id: string) => _getTweet(id),
-  ['tweet'],
+  ["tweet"],
   { revalidate: 3600 * 24 }
-)
- 
+);
+
 export const TweetComponent = async ({ id }: { id: string }) => {
   try {
-    const tweet = await getTweet(id)
-    return tweet ? <EmbeddedTweet tweet={tweet} /> : <TweetNotFound />
+    const tweet = await getTweet(id);
+    return tweet ? <EmbeddedTweet tweet={tweet} /> : <TweetNotFound />;
   } catch (error) {
-    console.error(error)
-    return <TweetNotFound error={error} />
+    console.error(error);
+    return <TweetNotFound error={error} />;
   }
-}
+};
