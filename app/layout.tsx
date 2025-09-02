0bf6245a6c9445f9333f7f3a19a140d12e04c149
RobPruzan <rapruzan@buffalo.edu>
2025-09-02 02:57:47 -0700

    donk

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import "./globals.css";
 import { AI_AGENT_NOTES, cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "./components/Navbar";
-import { ExperimentalTestingEntrypoint } from "../.xtra/app/entrypoint";
-import { UIWrapper } from "../.xtra/app/ui-wrapper";
 
 export const metadata: Metadata = {
   title: "Rob's blog",
@@ -36,39 +34,18 @@ export default function RootLayout({
             apiKey="KyyGrMdEBtKLD70b-06JjOOaxolZ2Gll"
             url="https://monitoring.react-scan.com/api/v1/ingest"
           /> */}
-        {process.env.NODE_ENV === "development" ? (
-          <UIWrapper enabled={false}>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              disableTransitionOnChange
-            >
-              <div className="flex min-h-screen flex-col bg-gradient-to-br from-[#0A0A0B] via-[#070707] to-[#0C0C0E] backdrop-blur-3xl">
-                <Navbar />
-                <main className="flex-1">{children}</main>
-              </div>
-            </ThemeProvider>
-            <Analytics />
-          </UIWrapper>
-        ) : (
-          <>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              disableTransitionOnChange
-            >
-              <div className="flex min-h-screen flex-col bg-gradient-to-br from-[#0A0A0B] via-[#070707] to-[#0C0C0E] backdrop-blur-3xl">
-                <Navbar />
-                <main className="flex-1">{children}</main>
-              </div>
-            </ThemeProvider>
-            <Analytics />
-          </>
-        )}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          <div className="flex min-h-screen flex-col bg-gradient-to-br from-[#0A0A0B] via-[#070707] to-[#0C0C0E] backdrop-blur-3xl">
+            <Navbar />
+            <main className="flex-1">{children}</main>
+          </div>
+        </ThemeProvider>
+        <Analytics />
       </body>
-      {process.env.NODE_ENV === "development" && (
-        <ExperimentalTestingEntrypoint />
-      )}
     </html>
   );
 }
